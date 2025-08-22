741294471ef0d30682e808550bd3b16012241408
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-08-22 21:21:25 +0300

    add-and-remove-task-functional

---
+++ ToDo/ToDo/MyTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo;
+
+public class MyTask
+{
+    private int id;
+    private bool comleted;
+    private string taskContent;
+
+    public MyTask(int id, string taskContent, bool completed)
+    {
+
+        this.id = id;
+        this.taskContent = taskContent;
+        this.comleted = completed;
+    }
+
+    public int Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public bool Comleted
+    {
+        get { return this.comleted; }
+        set { this.comleted = value; }
+    }
+
+    public string TaskContent
+    {
+        get { return this.taskContent; }
+        set { this.taskContent = value; }
+    }
+
+    public void printTask()
+    {
+        Console.WriteLine($"#{this.id}    {this.taskContent}    {this.comleted}");
+    }
+}
--- ToDo/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4611de61c17d715e63279bb56e5b0a16389fd74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c7ae82e4711e605a7592d042879f0af9f2ee34")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
